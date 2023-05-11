c808e376f6673494b1fe63355335435d3b425f44
Camilo <ecamiloinacio@gmail.com>
2023-05-11 15:25:04 -0300

    Exemplo de um CRUD, sem integração com BD

---
+++ crud/src/view/InicialPanel.java
@@ -0,0 +1,27 @@
+package view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class InicialPanel extends JPanel {
+	private AppFrame frame;
+	private JButton tarefasBtn;
+
+	public InicialPanel(AppFrame appFrame) {
+		frame = appFrame;
+
+		tarefasBtn = new JButton("Tarefas");
+		tarefasBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				frame.mostrarListaTarefas();
+			}
+		});
+		add(tarefasBtn);
+	}
+
+} // fim da class InicialPanel
---
+++ crud/src/view/TarefaTableModel.java
@@ -0,0 +1,83 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import model.Tarefa;
+
+public class TarefaTableModel extends AbstractTableModel {
+	private List<Tarefa> tarefas = new ArrayList<Tarefa>();
+	private String[] colunas = new String[] { "Id", "Nome", "Descrição" };
+
+	public TarefaTableModel(List<Tarefa> tarefas) {
+		this.tarefas = tarefas;
+	}
+
+	@Override
+	public int getRowCount() {
+		return tarefas.size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		return colunas.length;
+	}
+
+	@Override
+	public String getColumnName(int columnIndex) {
+		String columnName = null;
+
+		if (columnIndex >= 0 && columnIndex < colunas.length) {
+			columnName = colunas[columnIndex];
+		}
+		return columnName;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		String value = null;
+
+		if (rowIndex >= 0 && rowIndex < tarefas.size()) {
+			Tarefa tarefa = tarefas.get(rowIndex);
+
+			switch (columnIndex) {
+			case 0:
+				value = Integer.toString(tarefa.getId());
+				break;
+			case 1:
+				value = tarefa.getNome();
+				break;
+			case 2:
+				value = tarefa.getDescricao();
+				break;
+			default:
+				System.err.printf("[ERRO] Índice de coluna inválido: %d\n", columnIndex);
+			}
+		}
+
+		return value;
+	}
+
+	public Tarefa getTarefa(int rowIndex) {
+		Tarefa tarefa = null;
+
+		if (rowIndex >= 0 && rowIndex < tarefas.size()) {
+			tarefa = tarefas.get(rowIndex);
+		}
+
+		return tarefa;
+	}
+
+	public void carregar(List<Tarefa> tarefas) {
+		this.tarefas = tarefas;
+		fireTableDataChanged();
+	}
+
+	public void remover(Tarefa tarefa) {
+		tarefas.remove(tarefa);
+		fireTableDataChanged();
+	}
+
+} // fim da classe TarefaTabelaModel
